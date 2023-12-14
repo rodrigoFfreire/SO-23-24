672e13c7c6db67e150e2be4fe6d1c59ef9c76a7d
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2023-12-14 23:28:57 +0000

    fix: thread_delays had wrong allocation size

--- Part1/src/main.c
+++ Part1/src/main.c
@@ -51,7 +51,7 @@ int process_job(char *job_filepath, char *out_filepath, unsigned int access_dela
 
   pthread_mutex_t parseMutex = PTHREAD_MUTEX_INITIALIZER;
   char *thread_waits = (char*) malloc(sizeof(char) * max_threads);
-  unsigned int *thread_delays = (unsigned int*) malloc(sizeof(char) * max_threads);
+  unsigned int *thread_delays = (unsigned int*) malloc(sizeof(int) * max_threads);
 
   if (thread_waits == NULL || thread_delays == NULL) {
     fprintf(stderr, "Failed to allocat memory\n");

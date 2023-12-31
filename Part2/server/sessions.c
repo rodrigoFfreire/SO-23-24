5d1bc16e1e730918311f05a236f50354283aa5a8
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2023-12-31 19:42:53 +0000

    feat: Implemented Server-Side
    Finishes d9acf08

--- Part2/server/sessions.c
+++ Part2/server/sessions.c
@@ -6,6 +6,8 @@
 
 #include "queue.h"
 #include "sessions.h"
+#include "common/io.h"
+#include "operations.h"
 
 
 int check_termination(ConnectionQueue_t *queue) {
@@ -18,11 +20,86 @@ int check_termination(ConnectionQueue_t *queue) {
   return 0;
 }
 
+int read_requests(int req_fd, int resp_fd) {
+  char op = OP_NONE;
+  int return_value = 0;
+
+  while (op != OP_QUIT) {
+    if (safe_read(req_fd, &op, sizeof(char)) < 0) {
+      return 1;
+    }
+
+    switch (op) {
+    case OP_QUIT:
+      break;
+
+    case OP_CREATE:
+      char c_buf[CREATE_BUFSIZE];
+      if (safe_read(req_fd, c_buf + sizeof(char), CREATE_BUFSIZE - 1) < 0)
+        return 1;
+
+      return_value = ems_create(*(unsigned int*)(c_buf + sizeof(int)),
+                                *(size_t*)(c_buf + sizeof(size_t)), 
+                                *(size_t*)(c_buf + 2 * sizeof(size_t)));
+      
+      if (safe_write(resp_fd, &return_value, sizeof(int)) < 0) {
+        return 1;
+      }
+      
+      break;
+
+    case OP_RESERVE:
+      char r_buf[RESERVE_BUFSIZE];
+      if (safe_read(req_fd, r_buf + sizeof(char), 2 * sizeof(int) + sizeof(size_t) - 1) < 0)
+        return 1;
+
+      size_t num_seats = *(size_t*)(r_buf + sizeof(size_t));
+
+      if (safe_read(req_fd, r_buf + 2 * sizeof(size_t), 2 * num_seats * sizeof(size_t)) < 0)
+        return 1;
+
+      return_value = ems_reserve(*(unsigned int*)(r_buf + sizeof(int)),
+                                num_seats,
+                                (size_t*)(r_buf + 2 * sizeof(size_t)),
+                                (size_t*)(r_buf + sizeof(size_t) * (num_seats + 2)));
+
+      if (safe_write(resp_fd, &return_value, sizeof(int)) < 0)
+        return 1;
+
+      break;
+
+    case OP_SHOW:
+      char s_buf[SHOW_BUFSIZE];
+      if (safe_read(req_fd, s_buf + sizeof(char), SHOW_BUFSIZE - 1) < 0)
+        return 1;
+
+      return_value = ems_show(resp_fd, *(unsigned int*)(s_buf + sizeof(int)));
+
+      if (safe_write(resp_fd, &return_value, sizeof(int)) < 0)
+        return 1;
+
+      break;
+
+    case OP_LIST:
+      return_value = ems_list_events(resp_fd);
+
+      if (safe_write(resp_fd, &return_value, sizeof(int)) < 0)
+        return 1;
+
+      break;
+    
+    default:
+      break;
+    }
+  }
+  
+  return 0;
+}
 
 void *connect_clients(void *args) {
-  Session_t session_info = *(Session_t*)args;
-  unsigned int session_id = session_info.session_id;
-  ConnectionQueue_t *queue = session_info.queue;
+  Session_t *session_info = (Session_t*)args;
+  unsigned int session_id = session_info->session_id;
+  ConnectionQueue_t *queue = session_info->queue;
 
   char req_pipe_path[MAX_PIPE_NAME_SIZE];
   char resp_pipe_path[MAX_PIPE_NAME_SIZE];
@@ -54,7 +131,35 @@ void *connect_clients(void *args) {
     free(connection);
 
     fprintf(stdout, "\x1b[1;94m[WORKER %.2u] Connected to Client!\n", session_id);
-    sleep(4);
+
+    int resp_pipe = open(resp_pipe_path, O_WRONLY);
+    if (resp_pipe < 0) {
+      fprintf(stderr, "Failed opening response pipe\n");
+      continue;
+    }
+
+    if (safe_write(resp_pipe, &session_id, sizeof(int)) < 0) {
+      fprintf(stderr, "Failed to send response to client\n");
+      close(resp_pipe);
+      continue;
+    }
+
+    int req_pipe = open(req_pipe_path, O_RDONLY);
+    if (req_pipe < 0) {
+      fprintf(stderr, "Failed opening request pipe\n");
+      close(resp_pipe);
+      continue;
+    }
+
+    if (read_requests(req_pipe, resp_pipe)) {
+      fprintf(stderr, "Failed reading client requests\n");
+      close(resp_pipe);
+      close(req_pipe);
+      continue;
+    }
+
+    close(req_pipe);
+    close(resp_pipe);
     fprintf(stdout, "\x1b[1;94m[WORKER %.2u] Client disconnected [JOB DONE]\n", session_id);
   }
 
--- Part2/server/sessions.h
+++ Part2/server/sessions.h
@@ -2,12 +2,17 @@
 #define CONNECTIONS_H
 
 #include "queue.h"
+#include "common/constants.h"
 
+#define CREATE_BUFSIZE ((size_t) 3 * sizeof(size_t))
+#define RESERVE_BUFSIZE ((size_t) sizeof(size_t) * (2 * MAX_RESERVATION_SIZE + 2))
+#define SHOW_BUFSIZE ((size_t) 2 * sizeof(int))
 
 typedef struct Session {
     ConnectionQueue_t *queue;
     unsigned int session_id;
 } Session_t;
+
 
 void *connect_clients(void *args);
 
